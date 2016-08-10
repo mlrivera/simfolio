027a99c7b9b07b5cc288fbced2b0a494b83d65b5
mlrivera <marialauren.rivera@mail.utoronto.ca>
2016-08-09 20:18:14 -0400

    Changes made on Aug 8

--- functions.php
+++ functions.php
@@ -107,7 +107,9 @@ function simfolio_scripts() {
 	wp_enqueue_script( 'simfolio-navigation', get_template_directory_uri() . '/js/navigation.js', array(), '20151215', true );
 
 	wp_enqueue_script( 'simfolio-skip-link-focus-fix', get_template_directory_uri() . '/js/skip-link-focus-fix.js', array(), '20151215', true );
-
+    
+    wp_enqueue_style( 'simfolio-fonts', 'https://fonts.googleapis.com/css?family=Lemon' );
+    
 	if ( is_singular() && comments_open() && get_option( 'thread_comments' ) ) {
 		wp_enqueue_script( 'comment-reply' );
 	}
